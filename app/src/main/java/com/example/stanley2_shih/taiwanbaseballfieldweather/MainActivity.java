60fc507539512c9c1dfdcf06ae9673e5cce9301d
stanley_shih <Stanley2_Shih@asus.com>
2018-01-18 11:04:27 +0800

    show baseball field weather by web view

---
+++ app/src/main/java/com/example/stanley2_shih/taiwanbaseballfieldweather/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.stanley2_shih.taiwanbaseballfieldweather;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //The default web browser opens and loads the destination URL.
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("http://www.cwb.gov.tw/V7/forecast/entertainment/other/K001.htm");
+    }
+
+    private class MyWebViewClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.d("stanley","url = "+url);
+        }
+    }
+}
